a599ccdf579b4faf8dad404da60b8eab829f5ffb
samuel <samue.dinho@gmail.com>
2025-02-17 21:56:40 -0300

    criando pagina

---
+++ Projeto Individual de Desenvolvimento Web - 5º Período ADS/js/login.js
@@ -0,0 +1,27 @@
+login = "samuel@gmail.com";
+senha = "12345";
+$(document).ready(function() {
+    $('#loginForm').submit(function(event) {
+        event.preventDefault(); 
+
+        var email = $('#InputEmail1').val();
+        var password = $('#InputPassword1').val();
+
+        
+        if (email === "" || password === "") {
+            $('#error-message').text('Usuário e senha são obrigatórios!').show();
+            setTimeout(() =>{
+                $('#error-message').hide();
+            }, 3000);
+            
+        }else{
+            if(email === login & password === senha){
+                $('#sucess').text('Login bem sucedido!').show();
+            setTimeout(() => {
+                $('#sucess').hide();
+                window.location.href = "../index.html";
+            }, 3000);
+            }
+        }        
+    });
+});
