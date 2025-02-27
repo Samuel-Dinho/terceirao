644775c843439fd428a73aa426f544c615dd471b
SAMUEL MIGLIO MAIA JUNIOR <samuel_maia-junior@estudante.sesisenai.org.br>
2025-02-26 21:30:23 -0300

    atualizar

--- Projeto Individual de Desenvolvimento Web - 5º Período ADS/js/cadastro.js
+++ Projeto Individual de Desenvolvimento Web - 5º Período ADS/js/cadastro.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     auth = localStorage.getItem("login");
     login = document.getElementById('login');
+    
     if (auth){
         $('#criar').hide();
         $('#entrar').hide();
@@ -10,13 +11,26 @@ $(document).ready(function() {
         $('#entrar').show();
         $('#sair').hide();
     }
-
     $( "#sair" ).on( "click", function() {
         localStorage.setItem("login", "");
-        console.log(window.location.href);
+        localStorage.setItem("senha", "");
         setTimeout(() => {
-        //window.location.href = "index.html";
-        },1000);
-      } );
+            window.location.href = "/Users/samuel_maia-junior/Documents/terceirao/Projeto%20Individual%20de%20Desenvolvimento%20Web%20-%205º%20Período%20ADS/index.html"; // Redireciona para o index
+        }, 1000); 
+    });
+    $("#email").on("focusout", function(){
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        email = $('#inputEmail4').val();
+        message = $('#message');
+        if (emailRegex.test(email)) {
+            message.html('testo');
+            message.hide();
+        } else {
+            message.html('dsa');
+            message.show();
+        }
+
+    });
+    
       
 });
--- Projeto Individual de Desenvolvimento Web - 5º Período ADS/js/login.js
+++ Projeto Individual de Desenvolvimento Web - 5º Período ADS/js/login.js
@@ -1,17 +1,18 @@
 login = "samuel@gmail.com";
 senha = "12345";
+
 $(document).ready(function() {
     $('#loginForm').submit(function(event) {
         event.preventDefault(); 
 
-        var email = $('#InputEmail1').val();
-        var password = $('#InputPassword1').val();
+        email = $('#InputEmail1').val();
+        password = $('#InputPassword1').val();
 
         
         if (email === "" || password === "") {
-            $('#error-message').text('Usuário e senha são obrigatórios!').show();
+            $('#error').text('Usuário e senha são obrigatórios!').show();
             setTimeout(() =>{
-                $('#error-message').hide();
+                $('#error').hide();
             }, 2000);
             
         }else{
@@ -22,7 +23,51 @@ $(document).ready(function() {
                 window.location.href = "../index.html";
                 localStorage.setItem("login", email);
             }, 2000);
+            }else{
+                $('#InputEmail1').val('');
+                $('#InputPassword1').val('');
+                $('#error').text('Usuário ou senha estão erradas!').show();
+                setTimeout(() =>{
+                    $('#error').hide();
+                   
+                }, 2000);
             }
         }        
     });
+
+    $('#InputEmail1').on('change', function() {
+        if($('#exampleCheck1').is(':checked')){
+            localStorage.setItem("login", $('#InputEmail1').val());
+        }
+
+    });
+    $('#InputPassword1').on('change', function() {
+        if($('#exampleCheck1').is(':checked')){
+            localStorage.setItem("senha", $('#InputPassword1').val());
+        }
+
+    });
+    $('#exampleCheck1').on('change', function() {
+        if ($(this).is(':checked')) {
+            localStorage.setItem("login", $('#InputEmail1').val());
+            localStorage.setItem("senha", $('#InputPassword1').val());
+            localStorage.setItem("check", true);
+        } else {
+            localStorage.setItem("login", '');
+            localStorage.setItem("senha", '');
+            localStorage.setItem("check", false);
+
+        }
+    });
+    if(localStorage.getItem('check') == 'true'){
+        $('#exampleCheck1').prop('checked', true);
+        $('#InputPassword1').val(localStorage.getItem("senha"));
+        $('#InputEmail1').val(localStorage.getItem("login"));
+
+    }else{
+        localStorage.setItem("login", '');
+        localStorage.setItem("senha", '');
+        $('#InputPassword1').val('');
+        $('#InputEmail1').val('');
+    }
 });
